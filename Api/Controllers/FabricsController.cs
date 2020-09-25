dd81e3e7670043e0b4a7a818b7ad816c08ddd1ac
Muawiyya Modibbo Musa <modibo@exarotech.com>
2020-09-25 14:25:15 +0100

    AspIdentity

--- Api/Controllers/FabricsController.cs
+++ Api/Controllers/FabricsController.cs
@@ -5,45 +5,42 @@ using Domain;
 using Microsoft.AspNetCore.Mvc;
 using MediatR;
 using Application.Fabrics;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Api.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FabricsController : ControllerBase
-    {
-        private readonly IMediator _mediator;
-        public FabricsController(IMediator mediator)
-        {
-            _mediator = mediator;
 
-        }
+    public class FabricsController : BaseController
+    {
 
         [HttpGet]
-        public async Task<ActionResult<List<Fabric>>> List(){
-            return await _mediator.Send(new List.Query());
+        public async Task<ActionResult<List<Fabric>>> List()
+        {
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Fabric>> Details(Guid id){
-            return await _mediator.Send(new Details.Query{Id = id});
+        [Authorize]
+        public async Task<ActionResult<Fabric>> Details(Guid id)
+        {
+            return await Mediator.Send(new Details.Query { Id = id });
         }
 
         [HttpPost]
-        public async Task<ActionResult<Unit>> Create(Create.Command command){
-            return await _mediator.Send(command);
-        }
+        public async Task<ActionResult<Unit>> Create(Create.Command command) => await Mediator.Send(command);
 
         [HttpPut("{id}")]
-        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command){
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
             command.Id = id;
-            return await _mediator.Send(command);
+            return await Mediator.Send(command);
         }
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult<Unit>> Delete(Guid id){
-            
-            return await _mediator.Send(new Delete.Command{Id = id});
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+
+            return await Mediator.Send(new Delete.Command { Id = id });
         }
     }
 }
---
+++ Application/User/CurrentUser.cs
@@ -0,0 +1,44 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Persistence;
+
+namespace Application.User
+{
+    public class CurrentUser
+    {
+        public class Query : IRequest<User> { }
+
+        public class Handler : IRequestHandler<Query, User>
+        {
+            private readonly SJwtGenerator _jwtGenerator;
+            private readonly IUserAccessor _userAccessor;
+            private readonly UserManager<AppUser> _userManager;
+
+            public Handler(UserManager<AppUser> userManager, SJwtGenerator jwtGenerator,
+            IUserAccessor userAccessor)
+            {
+                this._userManager = userManager;
+
+                _userAccessor = userAccessor;
+                _jwtGenerator = jwtGenerator;
+
+            }
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await _userManager.FindByNameAsync(_userAccessor.GetCurrentUsername());
+
+                return new User
+                {
+                    DisplayName = user.DisplayName,
+                    Username = user.UserName,
+                    Token = _jwtGenerator.CreateToken(user),
+                    Image = null
+                };
+            }
+        }
+    }
+}
